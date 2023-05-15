b3216e9c3fe9bef91c672b270affeb1dc6ee20b7
Baliram <heyome9a@gmail.com>
2023-05-15 16:55:37 +0530

    ./app/_components/ProgramComp/ProgramAside.tsx:21:32 Type error: Type 'string[]' is not assignable to type 'codinasionDevApiType[]'.   Type 'string' is not assignable to type 'codinasionDevApiType'.

--- app/_components/CommonComp/AsideContributors.tsx
+++ app/_components/CommonComp/AsideContributors.tsx
@@ -1,21 +1,20 @@
 import { random } from "app/_functions/functions";
-import { codinasionDevApiType } from "app/_types/Devs";
 import BasicDevCard from "../DevsComp/Card/BasicDevCard";
 import Box from "../LayoutsComp/Box";
 
-export default function AsideContributors({ contributors }: { contributors: codinasionDevApiType[] }) {
+export default function AsideContributors({ contributors }: { contributors: string[] }) {
   return (
     <Box
       title={"Contributors"}
       style={`p-3 rounded-3xl p-2 bg-very-light lg:bg-white/50 dark:bg-very-dark dark:lg:bg-black/50`}>
       <div className="flex flex-wrap">
-        {contributors.map((item: codinasionDevApiType) =>
+        {contributors.map((item: string) =>
           <BasicDevCard
             key={random()}
             data={{
-              name: item.user.login,
-              avatar: `https://github.com/${item.user.login}.png`,
-              role: item.role
+              name: item,
+              avatar: `https://github.com/${item}.png`,
+              role: "contributor"
             }}
           />)}
       </div>
