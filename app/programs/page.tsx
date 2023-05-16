7eddff72ff4c68b001055d21bf92ee496e45c231
Baliram <heyome9a@gmail.com>
2023-05-16 08:48:27 +0530

    check import

---
+++ app/programs/page.tsx
@@ -0,0 +1,10 @@
+import Container from "app/_components/LayoutsComp/Container";
+
+export default function Page() {
+    return (<Container>
+        <div className="h-screen">
+
+            <h1 className="text-center w-full">checking components import</h1>
+        </div>
+    </Container>)
+}
