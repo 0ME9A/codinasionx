c9b8483a8f112c9462db0c887d4e49f989544121
Baliram <heyome9a@gmail.com>
2023-05-16 08:59:38 +0530

    file name changed and again import check

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -1,17 +1,11 @@
-import { Metadata } from "next";
-import HomeArticles from "app/_components/ProgramComp/HomeArticles";
-import HomeAside from "app/_components/ProgramComp/HomeAside";
+import Container from "app/_components/LayoutsComp/Container";
+import Nav from "app/_components/NavComp/ProjectNav";
 
-export const metadata: Metadata = {
-  title: `Program || Codinasion`,
-  description: `Share solutions with the community on Codinasion's Program repository. Find good first issues, make coordinated contributions, and join an inclusive community.`,
-};
-
-export default function Page() {
+export default async function Layout(): Promise<JSX.Element> {
   return (
-    <section className={"flex lg:container mx-auto p-2 gap-3 relative"}>
-      <HomeArticles />
-      <HomeAside />
-    </section>
+    <Container style="pt-20">
+      <Nav layout={{ name: `program`, slug: 'program' }} />
+      <h1 className="w-full text-black py-10 dark:text-white text-center">Components Import Check</h1>
+    </Container>
   )
 }
--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,10 +1,17 @@
-import Container from "app/_components/LayoutsComp/Container";
+import { Metadata } from "next";
+import HomeArticles from "app/_components/ProgramComp/HomeArticles";
+import HomeAside from "app/_components/ProgramComp/HomeAside";
+
+export const metadata: Metadata = {
+  title: `Program || Codinasion`,
+  description: `Share solutions with the community on Codinasion's Program repository. Find good first issues, make coordinated contributions, and join an inclusive community.`,
+};
 
 export default function Page() {
-    return (<Container>
-        <div className="h-screen">
-
-            <h1 className="text-center w-full">checking components import</h1>
-        </div>
-    </Container>)
+  return (
+    <section className={"flex lg:container mx-auto p-2 gap-3 relative"}>
+      <HomeArticles />
+      <HomeAside />
+    </section>
+  )
 }
